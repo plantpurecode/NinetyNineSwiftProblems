76891e56871e7e7cb83a04288ae38c7914a870ea
Jacob Relkin <jacobrelkin@gmail.com>
2018-07-14 18:49:18 +0300

    Initial Commit

---
+++ testingground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testingground
+//
+//  Created by Jacob Relkin on 7/14/18.
+//  Copyright © 2018 jacobrelkin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
