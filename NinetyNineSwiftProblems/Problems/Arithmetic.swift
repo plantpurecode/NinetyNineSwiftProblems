d56d46bb7de5ed64af7bd0f38045e607871a671f
Jacob Relkin <jacobrelkin@gmail.com>
2018-11-01 10:04:18 +0200

    Implemented problem #31: isPrime() and allPrime()

--- NinetyNineSwiftProblems/Problems/Arithmetic.swift
+++ NinetyNineSwiftProblems/Problems/Arithmetic.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+
+fileprivate func eratosthenesSieve(n: UInt) -> [UInt]? {
+    guard n > 1 else {
+        return nil
+    }
+    
+    let secondToLast = n.unsafeSubtracting(1)
+    var a = Array(repeating: false, count: 2)
+    a.append(contentsOf: Array(repeating: true, count: Int(secondToLast)))
+    
+    for i in 2..<a.count where a[i] {
+        var k = 2
+        while k*i < a.count {
+            a[k*i] = false
+            k += 1
+        }
+    }
+    
+    return a.enumerated().compactMap { i in
+        i.element == false ? nil : UInt(i.offset)
+    }
+}
+
+extension Int {
+    func isPrime() -> Bool {
+        return [self].allPrime()
+    }
+}
+
+extension Array where Element == Int {
+    func allPrime(greatestIndex: Int = -1) -> Bool {
+        return self.map { UInt($0) }.allPrime(greatestIndex: greatestIndex)
+    }
+}
+
+extension Array where Element == UInt {
+    
+    // TODO: Use Error type to bubble up error of too big index provided.
+    
+    func allPrime(greatestIndex: Int = -1) -> Bool {
+        let gi = greatestIndex == -1 ? count-1 : greatestIndex
+        guard gi < count else {
+            return false
+        }
+        
+        let greatest = self[gi]
+        
+        guard let sieve = eratosthenesSieve(n: greatest) else {
+            return false
+        }
+        
+        // If the sieve is equal, we're good.
+        if sieve == self {
+            return true
+        }
+        
+        // Check if the sieve and self fully intersect
+        let sieveSet = Set(sieve)
+        let selfSet = Set(self)
+        
+        return sieveSet.intersection(selfSet).count == count
+    }
+}
--- NinetyNineSwiftProblemsTests/Tests/ArithmeticTests.swift
+++ NinetyNineSwiftProblemsTests/Tests/ArithmeticTests.swift
@@ -9,6 +9,8 @@
 import XCTest
 @testable import NinetyNineSwiftProblems
 
+fileprivate let primes = [Int](arrayLiteral:2,3,5,7,11,13,17,19,23,29,31,37,41,43,47)
+
 class ArithmeticTests: XCTestCase {
 
     override func setUp() {
@@ -19,16 +21,19 @@ class ArithmeticTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testIsPrime() {
+        primes.forEach {
+            XCTAssertTrue($0.isPrime())
         }
     }
-
+    
+    func testAllPrime() {
+        XCTAssertTrue(primes.allPrime())
+        
+        let unorderedPrimes = [2,5,3,37,29,7]
+        XCTAssertTrue(unorderedPrimes.allPrime(greatestIndex: 3))
+        
+        // invalid index... should return false
+        XCTAssertFalse(unorderedPrimes.allPrime(greatestIndex: 10))
+    }
 }
