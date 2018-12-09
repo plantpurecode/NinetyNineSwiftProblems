ec5a77c890bca472dfd3f1492d85d72c348b731c
Jacob Relkin <jacobrelkin@gmail.com>
2018-12-09 19:06:06 +0200

    Renamed current and skipped problem files

--- navigate-to-current-problem.sh
+++ navigate-to-current-problem.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-current_problem_number=`cat .current-problem`
+current_problem_number=`cat current-problem`
 
 open "https://www.enekoalonso.com/projects/99-swift-problems/#p${current_problem_number}"
