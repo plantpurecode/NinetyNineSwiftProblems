3972e2acf754737732198ccdd60b36b1b09f51ac
Jacob Relkin <plantpurecode@gmail.com>
2019-02-10 01:57:26 +0200

    Section 6: Solved graph-term init problems for Graph & Digraph

--- NinetyNineSwiftProblems/Problems/Graph.swift
+++ NinetyNineSwiftProblems/Problems/Graph.swift
@@ -9,6 +9,26 @@
 import Foundation
 
 class Graph<T, U> {
+    class Node {
+        let value: T
+
+        init(value: T) {
+            self.value = value
+        }
+    }
+
+    class Edge {
+        let from: Node
+        let to: Node
+        let value: U
+
+        init(from: Node, to: Node, value: U) {
+            self.from = from
+            self.to = to
+            self.value = value
+        }
+    }
+
     enum Direction {
         case Indirected
         case Directed
@@ -18,10 +38,62 @@ class Graph<T, U> {
         return .Indirected
     }
 
-    var nodes: List<T>?
-    var edges: List<(T, T, U)>?
+    var nodes: List<Node>?
+    var edges: List<Edge>?
+}
+
+extension Graph where U == Int, T : Hashable {
+    convenience init(nodes n: List<T>, edges e: List<(T, T)>) {
+        self.init()
+
+        nodes = n.map { Node(value: $0) }.toList()
+
+        let nodesKeyedByValues = nodes!.reduce([T:Node]()) { (res, node) -> [T:Node] in
+            var r = res
+            r[node.value] = node
+            return r
+        }
+
+        func node(forValue value: T) -> Node? {
+            return nodesKeyedByValues[value]
+        }
+
+        // To track the edges we've already added to prevent against duplicates
+        var allEdges = Set<String>()
+
+        edges = e.compactMap {
+            guard let from = node(forValue: $0.0), let to = node(forValue: $0.1) else {
+                return nil
+            }
+
+            let nodeValues = [from, to].map { "\($0.value)" }
+            var edgeStrings = [nodeValues.joined(separator: "-")]
+            if type(of: self).direction == .Indirected {
+                edgeStrings.append(nodeValues.reversed().joined(separator: "-"))
+            }
+
+            guard edgeStrings.allSatisfy({ allEdges.contains($0) == false }) else {
+                return nil
+            }
+
+            edgeStrings.forEach { allEdges.insert($0) }
+            return Edge(from: from, to: to, value: 0)
+        }.toList()
+    }
 }
 
 class Digraph<T, U> : Graph<T, U> {
     override class var direction: Direction { return .Directed }
+}
+
+extension Graph.Node where T : CustomStringConvertible {
+    var description: String {
+        return "Graph.Node(value: \(value))"
+    }
+}
+
+extension Graph.Edge where T : CustomStringConvertible, U : CustomStringConvertible {
+    var description: String {
+        return "Graph.Edge(from: \(from.description)), to: \(to.description), value: \(value))"
+    }
 }
--- NinetyNineSwiftProblemsTests/Tests/GraphTests.swift
+++ NinetyNineSwiftProblemsTests/Tests/GraphTests.swift
@@ -11,5 +11,65 @@ import XCTest
 @testable import NinetyNineSwiftProblems
 
 class GraphTests : XCTestCase {
+    func testGraphTermInitialization() {
+        let graph = Graph<String, Int>(
+            nodes: List("b", "c", "d", "f", "g", "h", "k")!,
+            edges: List(("b", "c"), ("b", "f"), ("c", "f"), ("f", "k"), ("g", "h"), ("c", "f"), ("h", "g"), ("f", "b"))!
+        )
 
+        let expectedNodeValues = ["b", "c", "d", "f", "g", "h", "k"]
+        let expectedEdges = [
+            (from: "b", to: "c"),
+            (from: "b", to: "f"),
+            (from: "c", to: "f"),
+            (from: "f", to: "k"),
+            (from: "g", to: "h"),
+        ].map { (from: $0.from, to: $0.to, value: 0) }
+
+        let nodeValues = graph.nodes!.values.map { $0.value }
+        let edgeDataTuples = graph.edges!.map({ (from: $0.from.value, to: $0.to.value, value: $0.value) })
+
+        XCTAssertEqual(nodeValues, expectedNodeValues)
+        XCTAssertEqual(expectedEdges.count, edgeDataTuples.count)
+
+        zip(expectedEdges, edgeDataTuples).forEach { tuple in
+            let (expectedEdgeTuple, edge) = tuple
+
+            XCTAssertEqual(edge.from, expectedEdgeTuple.from)
+            XCTAssertEqual(edge.to, expectedEdgeTuple.to)
+            XCTAssertEqual(edge.value, expectedEdgeTuple.value)
+        }
+    }
+
+    func testDigraphTermInitialization() {
+        let graph = Digraph<String, Int>(
+            nodes: List("b", "c", "d", "f", "g", "h", "k")!,
+            edges: List(("b", "c"), ("c", "b"), ("f", "b"), ("b", "f"), ("b", "f"), ("c", "f"), ("f", "k"), ("g", "h"), ("f", "b"))!
+        )
+
+        let expectedNodeValues = ["b", "c", "d", "f", "g", "h", "k"]
+        let expectedEdges = [
+            (from: "b", to: "c"),
+            (from: "c", to: "b"),
+            (from: "f", to: "b"),
+            (from: "b", to: "f"),
+            (from: "c", to: "f"),
+            (from: "f", to: "k"),
+            (from: "g", to: "h")
+        ].map { (from: $0.from, to: $0.to, value: 0) }
+
+        let nodeValues = graph.nodes!.values.map { $0.value }
+        let edgeDataTuples = graph.edges!.map({ (from: $0.from.value, to: $0.to.value, value: $0.value) })
+
+        XCTAssertEqual(nodeValues, expectedNodeValues)
+        XCTAssertEqual(expectedEdges.count, edgeDataTuples.count)
+
+        zip(expectedEdges, edgeDataTuples).forEach { tuple in
+            let (expectedEdgeTuple, edge) = tuple
+
+            XCTAssertEqual(edge.from, expectedEdgeTuple.from)
+            XCTAssertEqual(edge.to, expectedEdgeTuple.to)
+            XCTAssertEqual(edge.value, expectedEdgeTuple.value)
+        }
+    }
 }
