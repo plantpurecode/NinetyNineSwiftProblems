4550f0aa8743cb7ee9d06d8e6313736d5c69b151
Jacob Relkin <plantpurecode@gmail.com>
2019-02-11 00:16:54 +0200

    Refactored graph-term init tests

--- NinetyNineSwiftProblemsTests/Tests/GraphTests.swift
+++ NinetyNineSwiftProblemsTests/Tests/GraphTests.swift
@@ -11,28 +11,15 @@ import XCTest
 @testable import NinetyNineSwiftProblems
 
 class GraphTests : XCTestCase {
-    func testGraphTermInitialization() {
-        let graph = Graph<String, Int>(
-            nodes: List("b", "c", "d", "f", "g", "h", "k")!,
-            edges: List(("b", "c"), ("b", "f"), ("c", "f"), ("f", "k"), ("g", "h"), ("c", "f"), ("h", "g"), ("f", "b"))!
-        )
-
-        let expectedNodeValues = ["b", "c", "d", "f", "g", "h", "k"]
-        let expectedEdges = [
-            (from: "b", to: "c"),
-            (from: "b", to: "f"),
-            (from: "c", to: "f"),
-            (from: "f", to: "k"),
-            (from: "g", to: "h"),
-        ].map { (from: $0.from, to: $0.to, value: 0) }
-
+    private func _testGraphTerm(_ graph: Graph<String, Int>, nodes: [String], edges e: [(from: String, to: String)]) {
         let nodeValues = graph.nodes!.values.map { $0.value }
         let edgeDataTuples = graph.edges!.map({ (from: $0.from.value, to: $0.to.value, value: $0.value) })
+        let edges = e.map { (from: $0.from, to: $0.to, value: 0) }
 
-        XCTAssertEqual(nodeValues, expectedNodeValues)
-        XCTAssertEqual(expectedEdges.count, edgeDataTuples.count)
+        XCTAssertEqual(nodeValues, nodes)
+        XCTAssertEqual(edges.count, edgeDataTuples.count)
 
-        zip(expectedEdges, edgeDataTuples).forEach { tuple in
+        zip(edges, edgeDataTuples).forEach { tuple in
             let (expectedEdgeTuple, edge) = tuple
 
             XCTAssertEqual(edge.from, expectedEdgeTuple.from)
@@ -41,35 +28,41 @@ class GraphTests : XCTestCase {
         }
     }
 
+    func testGraphTermInitialization() {
+        let graph = Graph<String, Int>(
+            nodes: List("b", "c", "d", "f", "g", "h", "k")!,
+            edges: List(("b", "c"), ("b", "f"), ("c", "f"), ("f", "k"), ("g", "h"), ("c", "f"), ("h", "g"), ("f", "b"))!
+        )
+
+        _testGraphTerm(graph,
+                       nodes: ["b", "c", "d", "f", "g", "h", "k"],
+                       edges: [
+                        (from: "b", to: "c"),
+                        (from: "b", to: "f"),
+                        (from: "c", to: "f"),
+                        (from: "f", to: "k"),
+                        (from: "g", to: "h")
+            ]
+        )
+    }
+
     func testDigraphTermInitialization() {
         let graph = Digraph<String, Int>(
             nodes: List("b", "c", "d", "f", "g", "h", "k")!,
             edges: List(("b", "c"), ("c", "b"), ("f", "b"), ("b", "f"), ("b", "f"), ("c", "f"), ("f", "k"), ("g", "h"), ("f", "b"))!
         )
 
-        let expectedNodeValues = ["b", "c", "d", "f", "g", "h", "k"]
-        let expectedEdges = [
-            (from: "b", to: "c"),
-            (from: "c", to: "b"),
-            (from: "f", to: "b"),
-            (from: "b", to: "f"),
-            (from: "c", to: "f"),
-            (from: "f", to: "k"),
-            (from: "g", to: "h")
-        ].map { (from: $0.from, to: $0.to, value: 0) }
-
-        let nodeValues = graph.nodes!.values.map { $0.value }
-        let edgeDataTuples = graph.edges!.map({ (from: $0.from.value, to: $0.to.value, value: $0.value) })
-
-        XCTAssertEqual(nodeValues, expectedNodeValues)
-        XCTAssertEqual(expectedEdges.count, edgeDataTuples.count)
-
-        zip(expectedEdges, edgeDataTuples).forEach { tuple in
-            let (expectedEdgeTuple, edge) = tuple
-
-            XCTAssertEqual(edge.from, expectedEdgeTuple.from)
-            XCTAssertEqual(edge.to, expectedEdgeTuple.to)
-            XCTAssertEqual(edge.value, expectedEdgeTuple.value)
-        }
+        _testGraphTerm(graph,
+                       nodes: ["b", "c", "d", "f", "g", "h", "k"],
+                       edges: [
+                        (from: "b", to: "c"),
+                        (from: "c", to: "b"),
+                        (from: "f", to: "b"),
+                        (from: "b", to: "f"),
+                        (from: "c", to: "f"),
+                        (from: "f", to: "k"),
+                        (from: "g", to: "h")
+            ]
+        )
     }
 }
