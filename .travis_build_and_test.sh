f9b383155459157214de331d81886cf237494c6b
Jacob Relkin <plantpurecode@gmail.com>
2019-10-02 00:35:05 +0300

    Trying to cache dependencies on Travis CI again

--- .travis_build_and_test.sh
+++ .travis_build_and_test.sh
@@ -1,4 +1,3 @@
 set -o pipefail
 
-xcodebuild -project "NinetyNineSwiftProblems.xcodeproj" -scheme "NinetyNineSwiftProblems" build 2>&1 | xcbeautify
-NSUnbufferedIO=YES xcodebuild -project "NinetyNineSwiftProblems.xcodeproj" -scheme "NinetyNineSwiftProblems" test 2>&1 | xcbeautify
+xcodebuild -project "NinetyNineSwiftProblems.xcodeproj" -scheme "NinetyNineSwiftProblems" build test 2>&1 | xcbeautify
